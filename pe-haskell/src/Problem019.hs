5d82e4ce9bc550d06f506f2119245028a6db5342
Graeme Tank <graemet@comcast.net>
2018-12-10 23:29:26 -0500

    Use module Data.Time.

--- pe-haskell/src/Problem019.hs
+++ pe-haskell/src/Problem019.hs
@@ -1,22 +1,42 @@
 -- Counting Sundays
 
--- By the way, 1200 // 7 = 171.
+{-
+Using Data.Time allows for greater concision.
+
+By the way, 1200 // 7 = 171.
+-}
 
 module Problem019
     ( problem019
     ) where
 
+import Data.Time (addGregorianMonthsClip, fromGregorian, toGregorian)
+import Data.Time.Calendar.WeekDate (toWeekDate)
+
 -- Weekdays are numbered Mondays (1) to Sundays (7)
 (monday, sunday) = (1, 7)
 
 problem019 :: IO ()
-problem019 =
+problem019 = do
+  -- With Data.Time
   print .
-  length .
-  filter (\(day, (_, d, _)) -> day == sunday && d == 1) .
-  dropWhile (\(_, (_, _, y)) -> y < 1901) $
-  dayDates
+    length .
+    filter (\day -> let (_, _, wday) = toWeekDate day in wday == sunday) $
+    days
 
+  -- Without Data.Time
+  print .
+    length .
+    filter (\(day, (_, d, _)) -> day == sunday && d == 1) .
+    dropWhile (\(_, (_, _, y)) -> y < 1901) $
+    dayDates
+
+-- With Data.Time
+days = takeWhile (\day -> let (y, _, _) = toGregorian day in y < 2001) .
+       map (\m -> addGregorianMonthsClip m $ fromGregorian 1901 1 1) $
+       [0..]
+
+-- Without Data.Time
 dayDates = zip (cycle [monday .. sunday]) dates
 
 dates = [ (m, d, y) | y <- [1900 .. 2000],
