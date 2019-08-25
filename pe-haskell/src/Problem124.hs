f457cf6c1e2cd0283a86a457adb1b37239c7b716
Graeme Tank <graemet@umich.edu>
2019-08-25 19:56:03 -0400

    Problem 124 done

--- pe-haskell/src/Problem124.hs
+++ pe-haskell/src/Problem124.hs
@@ -1,7 +1,7 @@
 -- Ordered radicals
 
 {-
-
+Use a brute force approach.
 -}
 
 module Problem124
@@ -9,10 +9,18 @@ module Problem124
   , _E
   ) where
 
-import Data.Numbers.Primes (primes)
+import Data.List (group, sort)
+import Data.Numbers.Primes (primeFactors)
 
 problem124 :: IO ()
 problem124 = print $ _E 100000 10000
 
-_E nMax k = 8
+_E :: Int -> Int -> Int
+_E nMax k = snd .
+            head .
+            drop (k - 1) .
+            sort $
+            map (\n -> (rad n, n)) [1 .. nMax]
 
+rad :: Int -> Int
+rad = product . map head . group . primeFactors 
