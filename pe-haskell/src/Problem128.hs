9a34f716ee757422bef4d1fac82b3429c8e1500b
Graeme Tank <graemet@umich.edu>
2019-09-13 00:08:09 -0400

    Problem 128 done. [problem128]

--- pe-haskell/src/Problem128.hs
+++ pe-haskell/src/Problem128.hs
@@ -2,24 +2,22 @@
 
 {-
 
-The unit vectors:
-                     ( 0, 1, 1)
-            (-1, 0, 1)        ( 1, 1, 0)
-                     ( 0, 0, 0)
-            (-1,-1, 0)        ( 1, 0,-1)
-                     ( 0,-1,-1)
+To find a better algorithm than testing predicate PD(n) = 3 for every
+n > 0, I noticed (while passing the test that the 10th predicate tile
+n = 271) that every n that satisfies the predicate could either be an
+element in OEIS sequence A077588 or be 1 less than an element in that
+sequence: [1,2,8,19,20,37,61,128,217,271]. This reduced the n search
+space from 14.5e9 to 1.4e5.
 
-Sequence going up [(0,0,0), (0,1,1) ..]:
+Sequence going up:
   A077588: Maximum number of regions into which the plane is divided by
   n triangles:
-    1, 2, 8, 20, 38, 62, ...
-    a(n) = 3n^2 - 3n + 2
-    a(n) = a(n-1) + 6n - 6 (a(1) = 2)
+    1, 2, 8, 20, 38, 62, 92, 128, 170, 218, 272, 332 ...
+    a(n) = 3n^2 - 3n + 2, a(0) = 1
 
-Going ul from dl hex face:
-1,3,10,23,42,67 ...
-  a(n) = 3n^2 - 2n + 2, a(0) = 1
-
+See end comments for using an (i,j) coordinate system to find the 6
+tiles adjacent to tile n, where tile n is the jth tile in the ith
+ring.
 
 -}
 
@@ -31,74 +29,86 @@ module Problem128
 
 import Data.Array
 import Data.Numbers.Primes
+import qualified Data.Set as Set
 
 problem128 :: IO ()
-problem128 = print $ 42
+problem128 = print $ nthTile 2000
 
 _PD :: Integer -> Int
-_PD n = length . filter isPrime . map (abs . (n -)) . adjacents $ n
+_PD n = length . filter isPrime' . map (abs . (n -)) . adjacents $ n
 
+-- Use set primes' to more quickly determine primes: use isPrime'
+-- lookup instead of isPrime calculation.
+-- 834761 is the largest prime difference for the 2000th tile.
+primes' = Set.fromList . takeWhile (< 834762) $ primes
+isPrime' n = n `Set.member` primes'
+
+-- Return tile n, the nth tile with PD(n) == 3. 
 nthTile :: Int -> Integer
-nthTile nth =  fst . head .
+nthTile nth = fst . head .
               drop (nth - 1) .
-              -- take nth .
               filter (\(_,pd) -> pd == 3) .
-              map (\n -> (n, _PD n)) $ [2..]
+              map (\n -> (n, _PD n)) $
+              interleave a077588' a077588
 
--- Tile n grouped by ring i
-rings :: [[Integer]]
-rings = [1] : map ring [1..] where
-  ring i = [j0 .. jn] where
-    j0 = 3*i*i - 3*i + 2
-    jn = j0 + 6*i - 1
+-- The initial correct but slow algorithm that checks every n > 0.
+nthTile' nth = fst . head .
+              drop (nth - 1) .
+              filter (\(_,pd) -> pd == 3) .
+              map (\n -> (n, _PD n)) $
+              [1..]
 
--- The six tiles adjacent to tile n
+-- Interleave two lists.
+interleave :: [a] -> [a] -> [a]
+interleave xs = concatMap (\(x,y) -> [x,y]) . zip xs
+
+-- OEIS A077588
+a077588  = map ij02n [1..]
+a077588' = map pred a077588
+
+-- The six tiles adjacent to tile n. See below comments for adjacency
+-- examples that aided formula determinations.
 adjacents :: Integer -> [Integer]
 adjacents 1 = [2..7]
 adjacents n = map ij2n adj_ijs where
   (i,j) = n2ij n
-  rn = 6*i
+  -- rn = 6 * i  -- omitted for reading symmetry
   pij = if j `mod` i /= 0
-        then  (i-1, ( 1 + (j * (i-1)) `div` i) `mod` rn)
+        then  (i-1, ( 1 + (j * (i-1)) `div` i) `mod` (6 * (i-1)))
         else  (i+1, (-1 + (j * (i+1)) `div` i) `mod` (6 * (i+1)))
   adj_ijs = pij :
             [ (i-1,   0 + (j * (i-1)) `div` i),
-              (i  , (j-1) `mod` rn),
-              (i  , (j+1) `mod` rn),
+              (i  ,       (j-1)                `mod` (6 * i)),
+              (i  ,       (j+1)                `mod` (6 * i)),
               (i+1,   0 + (j * (i+1)) `div` i),
               (i+1,   1 + (j * (i+1)) `div` i) ]
 
--- tile n with coordinates (i,j)
+-- Tile n with coordinates (i,j)
 ij2n :: (Integer, Integer) -> Integer
 ij2n (i,j) = ij02n i + j
 
+-- The tiles going up (j = 0): 1,2,8,20,38,62...
 ij02n 0 = 1
 ij02n i = 3*i*i - 3*i + 2
 
--- (i,j) coordinates of tile n
+-- The (i,j) coordinates of tile n
 n2ij :: Integer -> (Integer, Integer)
 n2ij n = (i, j) where
   i = ((floor . sqrt . fromInteger $ 12*n - 15) + 3) `div` 6
   j = n - ij02n i
 
--- unit direction vectors
-u :: Array Integer [Integer]
--- direction:           u        ul        dl       d        dr       ur
-u = listArray (0,5) [[0,1,1],[-1,0,1],[-1,-1,0],[0,-1,-1],[1,0,-1],[1,1,0]]
-
-origin = [0,0,0]
-
--- "vector" addition of two lists
-vadd :: Integral int => [int] -> [int] -> [int]
-vadd v1 v2 = map (\i -> v1!!i + v2!!i) [0,1,2]
-
 
 {-
 
-tile n and coordinates and unit hex movement to go from n -> n+1
+Notes taken during exploration.
+
+I abandoned the triplet coordinates after I solved for adjacent tiles
+using the (i,j) coordinates.
+
+Tile n and coordinates and unit hex movement to go from n -> n+1:
 
       hex
-tile  move        trip     r, t
+tile  move        trip     i, j
   1   u       ( 0, 0, 0)  (0, 0)
 
   2   dl      ( 0, 1, 1)  (1, 0)    0,2
@@ -123,31 +133,32 @@ tile  move        trip     r, t
 
  20   dl      ( 0, 3, 3)  (3, 0)
 
-|trip| = 2i
+trip: sum( |x|, |y|, |z| ) = 2i
 
 n(0,0) = 1
 n(i,j) = 3i^2 - 3i + 2 + j | i > 0, 0 <= j < 3(i+1)^2 - 3(i+1) + 2
 
-rt(n): i = floor sqrt (12n - 15) + 3 `div` 6
+ij(n): i = floor sqrt (12n - 15) + 3 `div` 6
        j = n - (3i^2 -3i + 2)
 
 adjacent tiles:
   Let adj n = (n_i-1, n_i, n_i+1) refer to the number of adjacent tiles
   to tile n in rings i-1, i, and i+1.
 
-  Now i = rn `div` 6, where rn = number of tiles in ring i, 
-  so every ith tile j in ring i has only 1 adjacent ring (i-1) tile;
-  all other ring i tiles have two adjacent ring (i-1) tiles. 
-
   For example, in ring 3, for tiles n = [20,23,26,29,32,35],
   adj n = (1,2,3); for all other tiles n in ring 3, adj n = (2,2,2).
 
+  Now i = rn `div` 6, where rn = number of tiles in ring i, so every
+  ith tile j in ring i has only 1 adjacent ring (i-1) tile; all other
+  ring i tiles have two adjacent ring (i-1) tiles. As added notation,
+  let rn- = 6(i-1) and rn+ = 6(i+1).
+
   tile (i,j) adjacent to:
     inner ring: (i-1,  0 + j * (i-1) `div` i)
-                (i-1,  1 + j * (i-1) `div` i `mod` rn) | j `mod` i /= 0 
+                (i-1,  1 + j * (i-1) `div` i `mod` rn-) | j `mod` i /= 0 
     ith ring:   (i  , j-1 `mod` rn)
                 (i  , j+1 `mod` rn)
-    outer ring: (i+1, -1 + j * (i+1) `div` i `mod` rn) | j `mod` i == 0
+    outer ring: (i+1, -1 + j * (i+1) `div` i `mod` rn+) | j `mod` i == 0
                 (i+1,  0 + j * (i+1) `div` i)
                 (i+1,  1 + j * (i+1) `div` i)
 
@@ -167,7 +178,7 @@ adjacent tiles:
     10 (2, 2)
     11 (2, 3)
 
-  8 (2, 0):    * 
+  8 (2, 0):    
      2 (1, 0)
      9 (2, 1)
     19 (2,11)
@@ -217,13 +228,5 @@ adjacent tiles:
     20 (3, 0)
     60 (4,22)   0     j * (i+1) `div` i
     61 (4,23)   1
-
-
-
-coord transforms:
-trip(i,j) = ? (a,b,c) ?
-rt(x,y,z) = ? (i,j) ?
-
-
 
 -}
