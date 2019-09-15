d1bb42bfcbadd5de552ec3f04f2f2f1e20d482b1
Graeme Tank <graemet@umich.edu>
2019-09-15 16:37:05 -0400

    Tests pass. Algorithm too slow. [problem129]

--- pe-haskell/src/Problem129.hs
+++ pe-haskell/src/Problem129.hs
@@ -2,6 +2,9 @@
 
 {-
 
+The ns can only end in 1, 3, 7, or 9. The first n is 3, because
+gcd(1,n) = 1.
+
 -}
 
 module Problem129
@@ -10,12 +13,25 @@ module Problem129
     iAexceeds
   ) where
 
+import Data.Numbers.Primes
+
 problem129 :: IO()
-problem129 = print $ 42
+problem129 = print $ iAexceeds 100
 
 iAexceeds :: Integer -> Integer
-iAexceeds k = k
+iAexceeds k = fst . head .
+              dropWhile (\(_, an) -> an <= k) .
+              map (\n -> (n, _A n)) $ ns
 
 _A :: Integer -> Integer
-_A n = n
+_A n = fst . head .
+       dropWhile (\(_, rkmn) -> rkmn /= 0) .
+       map (\k -> (k, _R k `mod` n)) $ [2..]
+
+-- n > 1 | gcd(10,n) = 1
+ns :: [Integer]
+ns = tail $ concatMap (\i -> map (+ i) [1,3,7,9]) [0,10..]
+
+_R :: Integer -> Integer
+_R k = (10^k - 1) `div` 9
 
